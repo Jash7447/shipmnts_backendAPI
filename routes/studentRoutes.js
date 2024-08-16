1002341985449335dbd83b6de912be87351b5ee4
Jash7447 <jashparikh7447@gmail.com>
2024-08-16 12:30:46 +0530

    final

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -4,4 +4,5 @@ const studentController = require('../controllers/studentController');
 
 
 router.get('/students/:studentId/classrooms', studentController.viewClassrooms);
+router.get('/students/:studentId/classrooms/:classroomId/tasks', studentController.viewTasks);
 
